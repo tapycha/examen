fd49777ea13b1a6d4cb480bc924fea8c9daa5834
DoctorApp <vladyslav.gamalii@nerdpeople.com>
2020-05-25 00:37:21 -0700

    Initial Commit

---
+++ ExamenPhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExamenPhotoApp
+//
+//  Created by Andrew Peneznyk on 25.05.2020.
+//  Copyright © 2020 Andrew Penzenyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
